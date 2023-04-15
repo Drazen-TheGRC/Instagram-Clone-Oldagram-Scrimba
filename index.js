843aa121d0c3752f993a5f66ec418c6e94249276
Drazen Drinic <drazendrinic@hotmail.com>
2023-04-15 16:31:49 +0200

    add JS random posts

--- index.js
+++ index.js
@@ -28,3 +28,47 @@ const posts = [
     }
 ]
 
+
+/*
+let name = posts[0].name
+let username = posts[0].username
+let location = posts[0].location
+let avatar = posts[0].avatar
+let post = posts[0].post
+let comment = posts[0].comment
+let likes = posts[0].likes
+*/
+
+/* HTML elements */
+
+const avatarImg = document.getElementById("avatar-img")
+const avatarName = document.getElementById("avatar-name")
+const avatarLocation = document.getElementById("avatar-location")
+const postImg = document.getElementById("post-img")
+const likesCount = document.getElementById("likes-count")
+
+const username = document.getElementById("username")
+const commentText = document.getElementById("comment-text")
+
+function setPost(postNumber){
+    avatarImg.src = posts[postNumber].avatar
+    avatarName.innerHTML = posts[postNumber].name
+    avatarLocation.innerHTML = posts[postNumber].location
+    postImg.src = posts[postNumber].post
+    likesCount.innerHTML = posts[postNumber].likes + " likes"
+    username.innerHTML = posts[postNumber].username + " "
+    commentText.innerHTML = posts[postNumber].comment
+}
+
+let currentPostNumber = Math.floor(Math.random() * posts.length)
+
+setPost(currentPostNumber)
+
+avatarImg.addEventListener("click", function(){
+    if(currentPostNumber === posts.length -1){
+        currentPostNumber = 0
+    }else{
+        currentPostNumber++
+    }
+    setPost(currentPostNumber)
+})
